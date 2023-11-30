48bf3482855cc36239d7e8404a9418e91b78ce69
Abylai1812 <abylaikhhann@mail.ru>
2023-11-30 17:57:26 +0500

    telebot

---
+++ main.py
@@ -0,0 +1,10 @@
+import telebot
+from env import BOT_TOKEN
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands = ['info'])
+def info(message):
+    bot.send_message(message.chat.id,'Привет! Я бот созданный для работы ученика группы Python-16 Тогамысов Абулхаира')
+
+bot.polling()
